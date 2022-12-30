a26d9a67e2451d9bcc145ba484af5b107b7d4095
Bruna Lima <bruna.b.santos@capgemini.com>
2022-12-30 13:30:08 -0300

    [US01] - Create user in firestore

---
+++ getpet_app/lib/model/user_model.dart
@@ -0,0 +1,29 @@
+// ignore: file_names
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class UserModel extends Model {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? firebaseUser;
+  Map<String, dynamic> userData = {};
+
+  void signUp(
+      {required Map<String, dynamic> userData, required String password}) {
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData["email"], password: password)
+        .then((auth) async {
+      firebaseUser = auth.user!;
+      _saveUserData(userData);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc("{firebaseUser!.uid}")
+          .get();
+    });
+  }
+}
+
+class _saveUserData {
+  _saveUserData(Map<String, dynamic> userData);
+}
