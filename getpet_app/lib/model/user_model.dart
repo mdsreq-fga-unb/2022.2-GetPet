3850be6a3a0519bc2775a70f493c0f481405e233
Bruna Lima <bruna.b.santos@capgemini.com>
2023-01-02 19:30:28 -0300

    [US01] Refactoring

---
+++ getpet_app/lib/model/user_model.dart
@@ -0,0 +1,50 @@
+// ignore: file_names
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class UserModel extends Model {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? firebaseUser;
+  Map<String, dynamic> userData = {};
+
+  bool isLoading = false;
+
+  void signUp({
+    required Map<String, dynamic> user,
+    required String password,
+    required VoidCallback onSuccess,
+    required VoidCallback onFail,
+  }) {
+    isLoading = true;
+    notifyListeners();
+    _auth
+        .createUserWithEmailAndPassword(
+            email: user["email"], password: password)
+        .then((auth) async {
+      firebaseUser = auth.user!;
+      _saveUserData(user);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc("{firebaseUser!.uid}")
+          .get();
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
+  }
+
+  Future _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(firebaseUser!.uid)
+        .set(userData);
+  }
+}
