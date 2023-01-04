161b0a40c18bdbf5c638c9b649e73af9f0a840f3
Bruna Lima <bruna.b.santos@capgemini.com>
2023-01-04 18:15:25 -0300

    [US01] Added home in main

--- getpet_app/lib/main.dart
+++ getpet_app/lib/main.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:getpet_app/view/sign_up.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'model/client_model.dart';
+import 'model/User_Model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScopedModel<ClientModel>(
-        model: ClientModel(),
+    return ScopedModel<UserModel>(
+        model: UserModel(),
         child: MaterialApp(
-          title: 'GetPet App',
+          title: 'GetPet',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.orange,
           ),
+          home: const SignUp(),
         ));
   }
 }
