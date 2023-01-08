5a6171296a9c2e8ee3b6a2f44f3a3e6e49744aa3
murilopbs <murilopbsouto@gmail.com>
2023-01-08 17:31:51 -0300

    Adicionando a troca de número

--- getpet_app/lib/Telas/AlterarPerfil.dart
+++ getpet_app/lib/Telas/AlterarPerfil.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:getpet/Widgets/VerPerfil.dart';
 import 'package:getpet/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class AlterarPerfil extends StatefulWidget {
   const AlterarPerfil({Key? key}) : super(key: key);
@@ -62,6 +64,80 @@ class _AlterarPerfilState extends State<AlterarPerfil> {
                                   ),
                                 ),
                                 const SizedBox(height: 15,),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width - 50,
+                                  child: Column(
+                                    children: [
+                                      IntlPhoneField(
+                                        decoration: const InputDecoration(
+                                          labelText: 'Número de celular',
+                                          border: OutlineInputBorder(
+                                            borderSide: BorderSide(),
+                                          ),
+                                        ),
+                                        initialCountryCode: 'BR',
+                                        onChanged: (phone) {
+                                          numeroCelular = phone.completeNumber;
+                                        },
+                                      ),
+                                      Text("Atual: ${data["numero"]}"),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 15,),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width - 20,
+                                  height: 50,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white
+                                    ),
+                                    child: const Text("Atualizar", style: TextStyle(color: Colors.black87),),
+                                    onPressed: () async{
+                                      if(nomeControlador.text.isNotEmpty && (numeroCelular != "" && numeroCelular != "+55")){
+                                        await FirebaseFirestore.instance.collection("usuarios").doc(model.firebaseUser!.uid).update({
+                                          "usuario": nomeControlador.text,
+                                          "numero": numeroCelular
+                                        });
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(builder: (context) => const VerPerfil())
+                                        );
+                                      } else if(nomeControlador.text.isEmpty && (numeroCelular != "" && numeroCelular != "+55")){
+                                        await FirebaseFirestore.instance.collection("usuarios").doc(model.firebaseUser!.uid).update({
+                                          "numero": numeroCelular
+                                        });
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(builder: (context) => const VerPerfil())
+                                        );
+                                      } else if(nomeControlador.text.isNotEmpty && (numeroCelular == "" || numeroCelular == "+55")){
+                                        await FirebaseFirestore.instance.collection("usuarios").doc(model.firebaseUser!.uid).update({
+                                          "usuario": nomeControlador.text
+                                        });
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(builder: (context) => const VerPerfil())
+                                        );
+                                      }else{
+                                        _aviso();
+                                      }
+                                    },
+                                  ),
+                                ),
+                                const SizedBox(height: 8,),
+                                SizedBox(
+                                  width: MediaQuery.of(context).size.width - 20,
+                                  height: 50,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white
+                                    ),
+                                    child: const Text("Voltar", style: TextStyle(color: Colors.black87),),
+                                    onPressed: (){
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(builder: (context) => const VerPerfil())
+                                      );
+                                    },
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -76,6 +152,7 @@ class _AlterarPerfilState extends State<AlterarPerfil> {
         }
     );
   }
+
   void _aviso(){
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
       duration: Duration(seconds: 2),
