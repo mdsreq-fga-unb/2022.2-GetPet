6997f2b2fc2224eba7ec19f7b33107fb5c61a5b2
murilopbs <murilopbsouto@gmail.com>
2023-01-08 17:28:56 -0300

    Adicionando a estrutura para acessar o banco de dados

--- getpet_app/lib/Telas/AlterarPerfil.dart
+++ getpet_app/lib/Telas/AlterarPerfil.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:getpet/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class AlterarPerfil extends StatefulWidget {
   const AlterarPerfil({Key? key}) : super(key: key);
@@ -8,8 +11,36 @@ class AlterarPerfil extends StatefulWidget {
 }
 
 class _AlterarPerfilState extends State<AlterarPerfil> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController nomeControlador = TextEditingController();
+  String numeroCelular = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScopedModelDescendant<UserModel>(
+        builder: (context, child, model){
+          return Scaffold(
+            body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                future: FirebaseFirestore.instance.collection("usuarios").doc(model.firebaseUser!.uid).get(),
+                builder: (_, snapshot) {
+                  if (snapshot.hasError) return Text('Error = ${snapshot.error}');
+                  if (snapshot.hasData) {
+                    var data = snapshot.data!.data();
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 60,),
+
+                        ],
+                      ),
+                    );
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                }
+            ),
+          );
+        }
+    );
   }
 }
